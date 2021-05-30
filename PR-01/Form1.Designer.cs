a585a4209adcf9abde4351f4c49ae6d586940868
GabyAllende <gabrielaallende158@gmail.com>
2021-05-30 19:54:31 -0400

    GABY: Creacion de forms y menu

--- PR-01/Form1.Designer.cs
+++ PR-01/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace PR_01
+﻿using System;
+
+namespace PR_01
 {
     partial class Form1
     {
@@ -29,12 +31,9 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.rtxt_codigo = new System.Windows.Forms.RichTextBox();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.txt_filename = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.btn_save = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,8 +42,7 @@
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 89.26553F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.73446F));
-            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.rtxt_codigo, 0, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 41);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
@@ -53,79 +51,53 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(885, 482);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
-            // richTextBox1
+            // rtxt_codigo
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(783, 235);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(822, 500);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Run";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.rtxt_codigo.Location = new System.Drawing.Point(3, 3);
+            this.rtxt_codigo.Name = "rtxt_codigo";
+            this.rtxt_codigo.Size = new System.Drawing.Size(783, 235);
+            this.rtxt_codigo.TabIndex = 0;
+            this.rtxt_codigo.Text = "";
+            this.rtxt_codigo.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // saveFileDialog1
             // 
             this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
             // 
-            // textBox1
+            // btn_save
             // 
-            this.textBox1.Location = new System.Drawing.Point(793, 3);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(89, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // txt_filename
-            // 
-            this.txt_filename.Location = new System.Drawing.Point(87, 12);
-            this.txt_filename.Name = "txt_filename";
-            this.txt_filename.Size = new System.Drawing.Size(270, 20);
-            this.txt_filename.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 15);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "File Name:";
+            this.btn_save.Location = new System.Drawing.Point(903, 144);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 8;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(995, 526);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txt_filename);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_save);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
+
+       
+
+       
 
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rtxt_codigo;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox txt_filename;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_save;
     }
 }
 
--- PR-01/Form1.cs
+++ PR-01/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,34 +13,43 @@ namespace PR_01
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        
+        private string[] contents;
+        private string myFile;
+        
+        public Form1(string filename)
         {
+            this.myFile = filename;
+            this.contents = System.IO.File.ReadAllLines(this.myFile);
             InitializeComponent();
+            rtxt_codigo.Lines = contents;
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-            saveFileDialog1.InitialDirectory = @"C:\";      
-            saveFileDialog1.Title = "Save text Files";
-            saveFileDialog1.CheckFileExists = true;
-            saveFileDialog1.CheckPathExists = true;
-            saveFileDialog1.DefaultExt = "txt";
-            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
-            saveFileDialog1.FilterIndex = 2;
-            saveFileDialog1.RestoreDirectory = true;
-            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                txt_filename.Text = saveFileDialog1.FileName;
-            }
-        }
-
+        
+      
         private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
 
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            contents = rtxt_codigo.Lines;
+        }
+
+       
+
+      
+
+        private void btn_save_Click_1(object sender, EventArgs e)
+        {
+           
+            System.IO.File.WriteAllLines(this.myFile, this.contents);
+            MessageBox.Show("Se guardaron los cambios correctamente", "GUARDADO CORRECTAMENTE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
 
         }
